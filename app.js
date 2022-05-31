a74089bdebf66aa68b33ac8c3d178cb6c269b0a5
Satyam Rajvanshi <satyamr4252@gmail.com>
2022-05-31 13:23:49 +0530

    returnTo

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const reviewRoutes = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+
 });
 
 const db = mongoose.connection;
--- routes/users.js
+++ routes/users.js
@@ -36,9 +36,11 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 })
 
 router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', "Goodbye!");
+        res.redirect('/campgrounds');
+    });
 })
 
 module.exports = router;
