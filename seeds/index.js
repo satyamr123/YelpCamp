6fce2c4950f7c4815c6bac03e06e16cd9ddfc891
Satyam Rajvanshi <satyamr4252@gmail.com>
2022-06-02 20:33:11 +0530

    Creating controllers

--- seeds/index.js
+++ seeds/index.js
@@ -35,6 +35,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
+            author: '6295cb9def80f75cf007352f',
             title: `${sample(places)}, ${sample(descriptors)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             image: await seedImg(),
